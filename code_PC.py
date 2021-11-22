7f7d24639b53aad612ee3f09d0b420dce0029a7a
Agrim Gautam <76497782+cyber-panther@users.noreply.github.com>
2021-11-22 06:44:23 +0530

    made project compatible with laptop camera

--- code_PC.py
+++ code_PC.py
@@ -1,6 +1,30 @@
 import cv2 as cv
+import winsound
 
-capture = cv.VideoCapture(1)
+frequency = 2500  # Set Frequency To 2500 Hertz
+duration = 1000  # Set Duration To 1000 ms == 1 second
+time = 0
+
+
+def set_up():
+    global capture
+    global eye_cascade
+    global face_cascade
+
+    capture = cv.VideoCapture(0)
+    eye_cascade = cv.CascadeClassifier('haar_eye.xml')
+    face_cascade = cv.CascadeClassifier('haar_face.xml')
+
+
+def message(frame, message):
+    frame = cv.putText(frame, message, (5, frame.shape[0]-10), cv.FONT_HERSHEY_SIMPLEX,
+                       1, (0, 0, 255), 2, cv.LINE_AA)
+
+
+def Buzzer():
+    if(time > 30):
+        print(time)
+        winsound.Beep(frequency, 1000)
 
 
 def rescale_frame(frame, scale=1):
@@ -12,29 +36,47 @@ def rescale_frame(frame, scale=1):
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
 
-def change_res(width, heigth):
-    capture.set(3, width)
-    capture.set(4, heigth)
+def eye_tracker(roi_gray, fx, fy, frame):
+
+    global time
+
+    eye_rect = eye_cascade.detectMultiScale(
+        roi_gray, scaleFactor=1.1, minNeighbors=15)
+
+    for (x, y, w, h) in eye_rect:
+        x = fx+x
+        y = fy+y
+
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        frame = cv.putText(frame, 'Eye', (x+5, y-5), cv.FONT_HERSHEY_SIMPLEX,
+                           .5, (255, 255, 0), 1, cv.LINE_AA)
+
+    if(len(eye_rect) < 1):
+        message(frame, 'no eye open')
+        time = time + 2
+
+    elif(len(eye_rect) == 1):
+        message(frame, 'One eye open')
+        time = time + 1
+
+    elif(len(eye_rect) > 1):
+        message(frame, 'Both eyes open')
+        time = 0
 
 
-while True:
-    isTrue, frame = capture.read()
+def face_tracker(frame):
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-    eye_cascade = cv.CascadeClassifier('haar_eye.xml')
-    face_cascade = cv.CascadeClassifier('haar_face.xml')
 
     face_rect = face_cascade.detectMultiScale(
         gray, scaleFactor=1.1, minNeighbors=6)
 
     if(len(face_rect) < 1):
-        frame = cv.putText(frame, 'No face detected', (5, frame.shape[0]-10), cv.FONT_HERSHEY_SIMPLEX,
-                           1, (0, 0, 255), 2, cv.LINE_AA)
+        message(frame, 'No face detected')
 
     elif(len(face_rect) > 1):
-        frame = cv.putText(frame, 'only one face...', (5, frame.shape[0]-10), cv.FONT_HERSHEY_SIMPLEX,
-                           1, (0, 0, 255), 2, cv.LINE_AA)
+        message(frame, 'only one face...')
 
     else:
         for (fx, fy, w, h) in face_rect:
@@ -44,28 +86,27 @@ while True:
 
             roi_gray = gray[fy:fy + h, fx:fx + w]
 
-            eye_rect = eye_cascade.detectMultiScale(
-                roi_gray, scaleFactor=1.1, minNeighbors=15)
+            eye_tracker(roi_gray, fx, fy, frame)
 
-            if(len(eye_rect) > 1):
-                frame = cv.putText(frame, 'only one face...', (5, frame.shape[0]-10), cv.FONT_HERSHEY_SIMPLEX,
-                                   1, (0, 0, 255), 2, cv.LINE_AA)
 
-            for (x, y, w, h) in eye_rect:
-                cv.rectangle(roi_gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                x = fx+x
-                y = fy+y
-                cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                frame = cv.putText(frame, 'Eye', (x+5, y-5), cv.FONT_HERSHEY_SIMPLEX,
-                                   .5, (255, 255, 0), 1, cv.LINE_AA)
+def main():
 
-            cv.imshow("test", roi_gray)
+    set_up()
 
-    frame_resized = rescale_frame(frame)
-    cv.imshow("video", frame_resized)
+    while True:
+        isTrue, frame = capture.read()
 
-    if cv.waitKey(1) & 0xFF == ord('x'):
-        break
+        face_tracker(frame)
 
-capture.release()
-cv.destroyAllWindows()
+        frame_resized = rescale_frame(frame)
+        cv.imshow("video", frame_resized)
+
+        if cv.waitKey(1) & 0xFF == ord('x'):
+            break
+
+    capture.release()
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
