37bc6c81fa120ddc372159846e14222c4d68a77e
Agrim Gautam <76497782+cyber-panther@users.noreply.github.com>
2021-11-23 01:20:07 +0530

    added buzzer feature through gpio pins

--- code_pi.py
+++ code_pi.py
@@ -1,6 +1,35 @@
 import cv2 as cv
+import RPi.GPIO as GPIO
+import time 
 
-capture = cv.VideoCapture(1)
+timer = 0
+buzzer = 37
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(buzzer,GPIO.OUT)
+
+
+def set_up():
+    global capture
+    global eye_cascade
+    global face_cascade
+
+    capture = cv.VideoCapture(0)
+    eye_cascade = cv.CascadeClassifier('haar_eye.xml')
+    face_cascade = cv.CascadeClassifier('haar_face.xml')
+
+
+def message(frame, message):
+    frame = cv.putText(frame, message, (5, frame.shape[0]-10), cv.FONT_HERSHEY_SIMPLEX,
+                       1, (0, 0, 255), 2, cv.LINE_AA)
+
+
+def buzz():
+    if(timer > 30):
+        print(timer)
+        GPIO.output(buzzer,GPIO.LOW)
+        time.sleep(0.25)
+        GPIO.output(buzzer,GPIO.HIGH)
 
 
 def rescale_frame(frame, scale=1):
@@ -12,29 +41,49 @@ def rescale_frame(frame, scale=1):
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
 
-def change_res(width, heigth):
-    capture.set(3, width)
-    capture.set(4, heigth)
+def eye_tracker(roi_gray, fx, fy, frame):
+
+    global timer
+
+    eye_rect = eye_cascade.detectMultiScale(
+        roi_gray, scaleFactor=1.1, minNeighbors=15)
+
+    for (x, y, w, h) in eye_rect:
+        x = fx+x
+        y = fy+y
+
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        frame = cv.putText(frame, 'Eye', (x+5, y-5), cv.FONT_HERSHEY_SIMPLEX,
+                           .5, (255, 255, 0), 1, cv.LINE_AA)
+
+    if(len(eye_rect) < 1):
+        message(frame, 'no eye open')
+        timer = timer + 2
+
+    elif(len(eye_rect) == 1):
+        message(frame, 'One eye open')
+        timer = timer + 1
+
+    elif(len(eye_rect) > 1):
+        message(frame, 'Both eyes open')
+        timer = 0
+        
+    buzz()
 
 
-while True:
-    isTrue, frame = capture.read()
+def face_tracker(frame):
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-    eye_cascade = cv.CascadeClassifier('haar_eye.xml')
-    face_cascade = cv.CascadeClassifier('haar_face.xml')
 
     face_rect = face_cascade.detectMultiScale(
         gray, scaleFactor=1.1, minNeighbors=6)
 
     if(len(face_rect) < 1):
-        frame = cv.putText(frame, 'No face detected', (5, frame.shape[0]-10), cv.FONT_HERSHEY_SIMPLEX,
-                           1, (0, 0, 255), 2, cv.LINE_AA)
+        message(frame, 'No face detected')
 
     elif(len(face_rect) > 1):
-        frame = cv.putText(frame, 'only one face...', (5, frame.shape[0]-10), cv.FONT_HERSHEY_SIMPLEX,
-                           1, (0, 0, 255), 2, cv.LINE_AA)
+        message(frame, 'only one face...')
 
     else:
         for (fx, fy, w, h) in face_rect:
@@ -44,28 +93,27 @@ while True:
 
             roi_gray = gray[fy:fy + h, fx:fx + w]
 
-            eye_rect = eye_cascade.detectMultiScale(
-                roi_gray, scaleFactor=1.1, minNeighbors=15)
+            eye_tracker(roi_gray, fx, fy, frame)
 
-            if(len(eye_rect) > 1):
-                frame = cv.putText(frame, 'only one face...', (5, frame.shape[0]-10), cv.FONT_HERSHEY_SIMPLEX,
-                                   1, (0, 0, 255), 2, cv.LINE_AA)
 
-            for (x, y, w, h) in eye_rect:
-                cv.rectangle(roi_gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                x = fx+x
-                y = fy+y
-                cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                frame = cv.putText(frame, 'Eye', (x+5, y-5), cv.FONT_HERSHEY_SIMPLEX,
-                                   .5, (255, 255, 0), 1, cv.LINE_AA)
+def main():
 
-            cv.imshow("test", roi_gray)
+    set_up()
 
-    frame_resized = rescale_frame(frame)
-    cv.imshow("video", frame_resized)
+    while True:
+        isTrue, frame = capture.read()
 
-    if cv.waitKey(1) & 0xFF == ord('x'):
-        break
+        face_tracker(frame)
 
-capture.release()
-cv.destroyAllWindows()
+        frame_resized = rescale_frame(frame)
+        cv.imshow("video", frame_resized)
+
+        if cv.waitKey(1) & 0xFF == ord('x'):
+            break
+
+    capture.release()
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
